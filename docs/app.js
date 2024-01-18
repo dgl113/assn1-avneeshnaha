7ced64989ef69d9ee29ab9c30f9341971f93b6d1
avneeshnaha <nahaavneesh@gmail.com>
2024-01-17 23:15:54 -0800

    Make guest Message containing the subheading for the page, like Guest: Adam Smith; this will be a string expression using guestName

--- docs/app.js
+++ docs/app.js
@@ -2,3 +2,7 @@
 
 var guestName = prompt("Please enter the guest's name:");
 console.log('Guest Name:', guestName);
+
+var guestName = prompt("Please enter the guest's name:");
+var guestMessage = 'Guest: ' + guestName;
+console.log('Guest Message:', guestMessage);
