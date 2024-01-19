2f358819af1390744782a4e3789a91f63c252235
avneeshnaha <nahaavneesh@gmail.com>
2024-01-19 09:42:05 -0800

    Add prompt for number of nights

--- docs/app.js
+++ docs/app.js
@@ -3,58 +3,7 @@
 //variables
 
 let guestName = prompt('Please Enter Guest Name:');
-
-var guestName = prompt("Please enter the guest's name:");
-console.log('Guest Name:', guestName);
-
-var guestName = prompt("Please enter the guest's name:");
-var guestMessage = 'Guest: ' + guestName;
-console.log('Guest Message:', guestMessage);
-
-var guestName = prompt("Please enter the guest's name:");
-var numberOfNights = parseInt(
-  prompt('Please enter the number of nights for the hotel stay:')
+let guestMessage = 'Guest: ' + guestName;
+let numberofNights = parseInt(
+  prompt('Please Enter The Number Of Nights For Your Stay: ')
 );
-console.log('Guest Name:', guestName);
-console.log('Number of Nights:', numberOfNights);
-
-var guestName = prompt("Please enter the guest's name:");
-var numberOfNights = parseInt(
-  prompt('Please enter the number of nights for the hotel stay:')
-);
-var nightlyRate = parseFloat(
-  prompt('Please enter the nightly rate for the hotel room (e.g. $225):')
-);
-console.log('Guest Name:', guestName);
-console.log('Number of Nights:', numberOfNights);
-console.log('Nightly Rate:', nightlyRate);
-var pstRate = parseFloat(
-  prompt('Please enter the rate of the provincial sales tax (e.g. 8%):')
-);
-
-var gstRate =
-  parseFloat(
-    prompt('Please enter the rate of the goods and services tax (e.g. 5%):')
-  ) / 100;
-
-var subTotal = numberOfNights * nightlyRate;
-
-var pstAmount = pstRate * subTotal;
-
-var gstAmount = gstRate * subTotal;
-
-var total = subTotal + pstAmount + gstAmount;
-
-var nightlyRate = 225;
-var pstRate = 0.08;
-var gstRate = 0.05;
-
-console.log('Guest Name:', guestName);
-console.log('Number of Nights:', numberOfNights);
-console.log('Nightly Rate:', nightlyRate);
-console.log('PST Rate:', pstRate);
-console.log('GST Rate:', gstRate);
-console.log('Subtotal:', subTotal);
-console.log('PST Amount:', pstAmount);
-console.log('GST Amount:', gstAmount);
-console.log('Total:', total);
