26c4a38a6b548f9e2f353e45c50dfd759ad5042e
avneeshnaha <nahaavneesh@gmail.com>
2024-01-19 09:46:10 -0800

    Add nightlyrate by adding number of nights and rate

--- docs/app.js
+++ docs/app.js
@@ -7,3 +7,4 @@ let guestMessage = 'Guest: ' + guestName;
 let numberofNights = parseInt(
   prompt('Please Enter The Number Of Nights For Your Stay: ')
 );
+let nightlyRate = numberofNights * nightlyRate;
